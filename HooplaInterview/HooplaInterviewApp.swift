3b19b48549c5ebab7aa0f7f3fee7802490cab639
Andrew McLean <mclean14@gmail.com>
2025-01-07 09:55:18 -0500

    Initial Commit

---
+++ HooplaInterview/HooplaInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HooplaInterviewApp.swift
+//  HooplaInterview
+//
+//  Created by Andrew McLean on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HooplaInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
